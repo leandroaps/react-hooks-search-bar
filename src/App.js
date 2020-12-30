302266d3d9dc2950c4f08dae2dfaa29237b70c23
Leandro Aparecido de Siqueira <laparecido@uolinc.com>
2020-12-30 16:37:27 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
+const social = [
+  'Siri',
+  'Alexa',
+  'Google',
+  'Facebook',
+  'Twitter',
+  'Linkedin',
+  'Sinkedin',
+];
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    const results = social.filter((person) =>
+      person.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <nav class='navbar navbar-expand-md navbar-dark fixed-top bg-dark'>
+          <div class='container-fluid'>
+            <button
+              class='navbar-toggler'
+              type='button'
+              data-bs-toggle='collapse'
+              data-bs-target='#navbarCollapse'
+              aria-controls='navbarCollapse'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+            >
+              <span class='navbar-toggler-icon'></span>
+            </button>
+            <div class='collapse navbar-collapse' id='navbarCollapse'>
+              <form class='d-flex'>
+                <input
+                  class='form-control me-2'
+                  type='search'
+                  placeholder='Search'
+                  aria-label='Search'
+                  value={searchTerm}
+                  onChange={handleChange}
+                />
+                <button class='btn btn-outline-success' type='submit'>
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </nav>
       </header>
+
+      <main class='flex-shrink-0'>
+        <div class='container mt-5'>
+          <ul class='list-group'>
+            {searchResults.map((item) => (
+              <li class='list-group-item'>{item}</li>
+            ))}
+          </ul>
+        </div>
+      </main>
     </div>
   );
 }
