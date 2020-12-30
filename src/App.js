ae448e6bcc24db34eb616b6e8c7df93714df2d70
Leandro Aparecido de Siqueira <laparecido@uolinc.com>
2020-12-30 16:49:27 -0300

    Visual adjustments

--- src/App.js
+++ src/App.js
@@ -28,6 +28,9 @@ function App() {
       <header>
         <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
           <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              Search bar with React Hooks using useState
+            </a>
             <button
               className="navbar-toggler"
               type="button"
@@ -39,27 +42,23 @@ function App() {
             >
               <span className="navbar-toggler-icon" />
             </button>
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-              <form className="d-flex">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                  value={searchTerm}
-                  onChange={handleChange}
-                />
-                <button className="btn btn-outline-success" type="submit">
-                  Search
-                </button>
-              </form>
-            </div>
+            <div className="collapse navbar-collapse" id="navbarCollapse" />
           </div>
         </nav>
       </header>
 
       <main className="flex-shrink-0">
-        <div className="container mt-5">
+        <div className="container mt-2">
+          <form className="d-flex">
+            <input
+              className="form-control form-control-lg mb-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={searchTerm}
+              onChange={handleChange}
+            />
+          </form>
           <ul className="list-group">
             {searchResults.map((item) => (
               <li className="list-group-item" key={item}>
